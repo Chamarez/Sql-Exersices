415e3eb2d8d2d41c1a615ebc7e372e4e67ebffb3
chamarez <michel.chamarez@gmail.com>
2021-12-04 17:07:42 +0100

    Exc 2

--- ejercicio1.sql
+++ ejercicio1.sql
@@ -12,3 +12,17 @@ INSERT INTO curso VALUES (103, NULL,"","Tarde",30);
 INSERT INTO curso VALUES (102, "Algebra","","Tarde",30);
 UPDATE curso SET cupo=25;
 DELETE FROM curso WHERE nombre="Algoritmos";
+
+
+
+
+
+SELECT nombre, apellido, fecha_nacimiento FROM PROFESOR ORDER BY
+fecha_nacimiento;
+SELECT * FROM PROFESOR WHERE salario >= 65000;
+select * from PROFESOR where fecha_nacimiento between '1980-01-01' and
+'1989-12-31';
+SELECT * FROM PROFESOR LIMIT 5;
+SELECT * FROM PROFESOR WHERE apellido LIKE ‘P%’;
+SELECT * FROM PROFESOR WHERE fecha_nacimiento between '1980-01-01' and
+'1989-12-31' AND salario > 80000;
